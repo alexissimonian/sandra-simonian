5f190fabaa26cc8852778ed32de70408f5fe7789
Alexis Simonian <alexis.simonian@proton.me>
2025-11-12 09:22:43 +0100

    init variables.scss

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    allowedHosts: ['dev.alexissimonian.net']
+  },
+  css: {
+    preprocessorOptions: {
+      scss: { additionalData: `@use "/src/styles/_variables.scss" as *;`, }
+    }
+  }
 });
