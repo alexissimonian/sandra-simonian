e925fd1e939d546b50c9e3a9eec1101ba0f48517
Alexis Simonian <alexis.simonian@proton.me>
2025-11-15 10:58:13 +0100

    svar

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -14,10 +14,18 @@
 <h1 class="sr-only">Sandra Simonian - Admin Center</h1>
 <div class="faux-body">
   <h2>Hello, {data.profile?.name}</h2>
-  <Button onclick={() => goto(page.url.pathname + "/users")}
-    >Utilisateurs</Button
-  >
+  <div class="buttons-containers">
+    <Button onclick={() => goto("/app")}>App</Button>
+    <Button onclick={() => goto(page.url.pathname + "/users")}
+      >Utilisateurs</Button
+    >
+    <Button onclick={() => goto(page.url.pathname + "/rdv")}>RDV</Button>
+  </div>
 </div>
 
 <style lang="scss">
+  .buttons-containers {
+    display: flex;
+    gap: 1rem;
+  }
 </style>
