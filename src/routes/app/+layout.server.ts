50d649babfe7de03bd2a0c755d6149c6751e9c7d
Alexis <alexis.simonian@outlook.com>
2025-12-03 15:24:00 +0000

    go to admin if admin

--- src/routes/app/+layout.server.ts
+++ src/routes/app/+layout.server.ts
@@ -1,7 +1,12 @@
+import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types'
 
-export const load: LayoutServerLoad = async ({ locals, cookies }) => {
+export const load: LayoutServerLoad = async ({ locals, cookies, url }) => {
   const profile = locals.profile;
+
+  if (url.searchParams.get("source") === "login" && profile.role === "admin") {
+    throw redirect(303, "/admin");
+  }
 
   return {
     profile,
--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import type { PageData } from "./$types";
-
   export let data: PageData;
 </script>
 
@@ -8,5 +7,7 @@
   <title>Bienvenue sur votre espace coaching</title>
 </svelte:head>
 
-<h1 class="sr-only">Bienvenue sur votre espace coaching</h1>
-<h2>Bienvenue {data.profile?.firstname}</h2>
+<div class="faux-body">
+  <h1 class="sr-only">Bienvenue sur votre espace coaching</h1>
+  <h2>Bienvenue, {data.profile?.firstname}</h2>
+</div>
